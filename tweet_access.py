53a694633067240ebad6b78bd52bc34701a017ff
Eddie Jagger <eddiejcranmer@gmail.com>
2018-06-24 21:40:18 +0000

    twitter.py

--- tweet_access.py
+++ tweet_access.py
@@ -5,10 +5,7 @@ from collections import Counter
 from prettytable import PrettyTable
 
 
-CONSUMER_KEY = 'YTotqVnLO1VwR5OCn99UbvGdU'
-CONSUMER_SECRET = 'h3hCb6hZ0FJ8tP5a4rIRRgehvcsw42n8b4NjSXXi7K2CryHW0D'
-OAUTH_TOKEN = '17812083-h3CqJxJ7ppQaymwNNbONN4S2n7NXwX5L2pBsOjz0S'
-OAUTH_TOKEN_SECRET= 'BZGUl8ZhqQDuXXU1oaOMvItuiWRWQQAIZsRceEe1Xhw4G'
+'inser consumer keys here
 
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
